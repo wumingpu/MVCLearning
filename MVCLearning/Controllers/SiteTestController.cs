17347b657ca6d6a39a642b1a22b98e6be0c6557f
v-mingpw <v-mingpw@V-MINGPW-03>
2016-11-18 08:31:53 +0800

    Sync

---
+++ MVCLearning/Controllers/SiteTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLearning.Controllers
+{
+    public class SiteTestController : Controller
+    {
+        // GET: SiteTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcMusicStore/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public virtual int AlbumId { get; set; }
+        public virtual int  GenreId { get; set; }
+        public virtual int  ArtistId { get; set; }
+    }
+}
